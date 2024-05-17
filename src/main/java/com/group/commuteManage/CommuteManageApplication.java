103d1296a561e4a795b2cbff1557adfc90716990
Hyeri1ee <haerizian10@gmail.com>
2024-05-17 11:29:15 +0200

    [set] spring initializer 기본 세팅

---
+++ src/main/java/com/group/commuteManage/CommuteManageApplication.java
@@ -0,0 +1,13 @@
+package com.group.commuteManage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommuteManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommuteManageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group/commuteManage/CommuteManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.commuteManage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommuteManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
